153d8725b8bba6ddd8d1d3d4c04afeea581f4530
Jeff Bignell <jbignell@mrx.ca>
2013-02-15 12:27:23 -0500

    rest of the ribbit files

--- ribbit/urls.py
+++ ribbit/urls.py
@@ -10,7 +10,8 @@ urlpatterns = patterns('',
     url(r'^login$', 'ribbit_app.views.login_view'), # login
     url(r'^logout$', 'ribbit_app.views.logout_view'), # logout
     url(r'^ribbits$', 'ribbit_app.views.public'), # public ribbits
-    url(r'^submit$', 'ribbit_app.views.submit'), # submit new ribbit   
+    url(r'^submit$', 'ribbit_app.views.submit'), # submit new ribbit  
+    url(r'^signup$', 'ribbit_app.views.signup'), # signup 
     url(r'^users/$', 'ribbit_app.views.users'),
     url(r'^users/(?P<username>\w{0,30})/$', 'ribbit_app.views.users'),
     url(r'^follow$', 'ribbit_app.views.follow'), 
