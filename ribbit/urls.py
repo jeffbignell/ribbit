42a61906a035819bada715dbf45c07ec74d420a1
Jeff Bignell <jbignell@mrx.ca>
2013-02-15 10:32:50 -0500

    Implemented User Login and Sign up

--- ribbit/urls.py
+++ ribbit/urls.py
@@ -6,6 +6,10 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
+    url(r'^$', 'ribbit_app.views.index'), # root
+    url(r'^login$', 'ribbit_app.views.login_view'), # login
+    url(r'^logout$', 'ribbit_app.views.logout_view'), # logout
+    url(r'^signup$', 'ribbit_app.views.signup'), # signup
     # url(r'^$', 'ribbit.views.home', name='home'),
     # url(r'^ribbit/', include('ribbit.foo.urls')),
 
