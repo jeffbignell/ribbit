4f34ce13c4aa68635f0bf66ae6ed9dafaf005fe1
Jeff Bignell <jbignell@mrx.ca>
2013-02-15 12:09:19 -0500

    implmented user profiles and following relation on frontend

--- ribbit/urls.py
+++ ribbit/urls.py
@@ -10,7 +10,10 @@ urlpatterns = patterns('',
     url(r'^login$', 'ribbit_app.views.login_view'), # login
     url(r'^logout$', 'ribbit_app.views.logout_view'), # logout
     url(r'^ribbits$', 'ribbit_app.views.public'), # public ribbits
-    url(r'^submit$', 'ribbit_app.views.submit'), # submit new ribbit    
+    url(r'^submit$', 'ribbit_app.views.submit'), # submit new ribbit   
+    url(r'^users/$', 'ribbit_app.views.users'),
+    url(r'^users/(?P<username>\w{0,30})/$', 'ribbit_app.views.users'),
+    url(r'^follow$', 'ribbit_app.views.follow'), 
 # url(r'^$', 'ribbit.views.home', name='home'),
     # url(r'^ribbit/', include('ribbit.foo.urls')),
 
