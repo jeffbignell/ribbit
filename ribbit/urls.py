1afa37eb54a65b07c501ecf4ec0357c6ae355e3b
Jeff Bignell <jbignell@mrx.ca>
2013-02-15 11:16:35 -0500

    Implemented Ribbit submission and public ribbits views

--- ribbit/urls.py
+++ ribbit/urls.py
@@ -9,8 +9,9 @@ urlpatterns = patterns('',
     url(r'^$', 'ribbit_app.views.index'), # root
     url(r'^login$', 'ribbit_app.views.login_view'), # login
     url(r'^logout$', 'ribbit_app.views.logout_view'), # logout
-    url(r'^signup$', 'ribbit_app.views.signup'), # signup
-    # url(r'^$', 'ribbit.views.home', name='home'),
+    url(r'^ribbits$', 'ribbit_app.views.public'), # public ribbits
+    url(r'^submit$', 'ribbit_app.views.submit'), # submit new ribbit    
+# url(r'^$', 'ribbit.views.home', name='home'),
     # url(r'^ribbit/', include('ribbit.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
